f04a451a02aa6ba3ac815474cd3854fcf58b93de
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-12-01 03:15:17 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -72,7 +72,7 @@ def game_hash
         :points => 10,
         :rebounds => 1,
         :assists => 1,
-        :steals => 4,
+        :steals => 2,
         :blocks => 14,
         :slam_dunks => 100
         },
