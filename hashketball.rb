e8ec544276b3d815fd21e6146d111b3be3dc3031
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-12-01 02:35:49 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,2 +1,17 @@
 # Write your code here!
 
+def game_hash
+  hashketball = {
+    :home => {
+      :team_name = ,
+      :colors = ,
+      :players = 
+    },
+    :away => {
+      :team_name = ,
+      :colors = ,
+      :players = 
+    }
+    
+  }
+end 
