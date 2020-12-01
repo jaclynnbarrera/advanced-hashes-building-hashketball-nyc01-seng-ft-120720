fb65da37c789777f052fd9fbe6313132edfe610f
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-12-01 03:20:04 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -113,7 +113,7 @@ def game_hash
         :player_name => "Kemba Walker",
         :number => 33,
         :shoe => 15,
-        :points => 33,
+        :points => 6,
         :rebounds => 3,
         :assists => 2,
         :steals => 1,
