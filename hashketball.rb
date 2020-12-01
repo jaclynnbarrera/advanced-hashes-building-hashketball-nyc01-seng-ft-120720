b6d6a84e2bf2ab1c9c10bd8777ee3d3b1e162507
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-12-01 02:52:21 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,27 +1,7 @@
-# Write your code here!
 
 def game_hash
   hashketball = {
-    :home => {
-      :team_name = "Brooklyn Nets" ,
-      :colors = ["Black", "White"],
-      :players = [{
-        :player_name => "Jeff Adrien",
-        :number => ,
-        :shoe => ,
-        :points => ,
-        :rebounds => ,
-        :assists => ,
-        :steals => ,
-        :blocks => ,
-        :slam_dunks => ,
-      }]
-    },
-    :away => {
-      :team_name = ,
-      :colors = ,
-      :players = 
-    }
+    
     
   }
 end 
