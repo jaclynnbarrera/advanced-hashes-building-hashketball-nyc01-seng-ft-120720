21d864829abef112170f5f1110626a7a838fed41
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-12-01 03:12:58 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -51,7 +51,7 @@ def game_hash
         },
         {
         :player_name => "Jason Terry",
-        :number => 1,
+        :number => 31,
         :shoe => 19,
         :points => 26,
         :rebounds => 11,
