89d31d744bb5808f32ac4c870460c4090e38fd62
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-12-01 03:15:03 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -68,7 +68,7 @@ def game_hash
         {
         :player_name => "Jeff Adrien",
         :number => 4,
-        :shoe => "nike",
+        :shoe => 18,
         :points => 7,
         :rebounds => 17,
         :assists => 20,
