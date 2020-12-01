dec833681b597a1e39624da2bc24cf044cdeabb6
jaclyn barrera <jaclynnbarrera@gmail.com>
2020-12-01 03:05:20 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -9,7 +9,26 @@ def game_hash
     :away => {
       :team_name => "" ,
       :colors => ["array", "of", "strings"] ,
-      :players => [{},{},{}]
+      :players => [{
+        :player_name => "name",
+        :number => 7,
+        :shoe => "nike",
+        :points => 7,
+        :rebounds => 17,
+        :assists => 20,
+        :steals => 4,
+        :blocks => 14,
+        :slam_dunks => 100},{
+        :player_name => "name",
+        :number => 7,
+        :shoe => "nike",
+        :points => 7,
+        :rebounds => 17,
+        :assists => 20,
+        :steals => 4,
+        :blocks => 14,
+        :slam_dunks => 100
+        },{}]
     }
   }
 end 
